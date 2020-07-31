71052a27a37263f5d5c314c1b419fdd8b1833ef9
Ricardo izidorio Ferreira <42355541+rizidorio@users.noreply.github.com>
2020-07-30 21:21:19 -0300

    alteracao url heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -22,7 +22,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://rickflix.herokuapp.com/categorias';
     fetch(URL).then(async (data) => {
       const resposta = await data.json();
       setCategorias([
